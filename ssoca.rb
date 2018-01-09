b47a6efb5ac98dd06ba9d8ad943228b82aec42ad
Danny Berger <dpb587@gmail.com>
2018-01-09 12:29:25 -0800

    Bump ssoca to 0.8.0

--- ssoca.rb
+++ ssoca.rb
@@ -1,16 +1,16 @@
 class Ssoca < Formula
   desc "Client for SSO for services that use CA-based authentication."
   homepage "https://github.com/dpb587/ssoca"
-  version "0.7.1"
+  version "0.8.0"
 
   depends_on :arch => :x86_64
 
   if OS.mac?
     url "https://github.com/dpb587/ssoca/releases/download/v#{version}/ssoca-client-#{version}-darwin-amd64"
-    sha256 "6393442bd9002f0a89ef5dc81ae6936f84e248d0dea02a3098b26693b9dde78c"
+    sha256 "f83c50e2f1d4edeefe258ceac2c0c59a6b2f8dbcea95313d0442013911be8cc1"
   elsif OS.linux?
     url "https://github.com/dpb587/ssoca/releases/download/v#{version}/ssoca-client-#{version}-linux-amd64"
-    sha256 "adbb82453de3bc7e4cf66baf533089ee8f8b1d3a3cec15825a0dd0c488c08fc8"
+    sha256 "eeffed6bd82b95091eb4103f96fe802d691662f58a4649221fb48452c11bb0c6"
   end
 
   def install
