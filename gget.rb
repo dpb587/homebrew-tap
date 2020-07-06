ff08debed50b28ac1d0758b4a20a214e1b0309d0
Danny Berger <dpb587@gmail.com>
2020-07-06 08:46:02 -0700

    Update gget.rb

--- gget.rb
+++ gget.rb
@@ -1,16 +1,16 @@
 class Gget < Formula
   desc "A small utility for getting files from git repositories."
   homepage "https://github.com/dpb587/gget"
-  version "0.5.0"
+  version "0.5.2"
 
   depends_on :arch => :x86_64
 
   if OS.mac?
-    url "https://github.com/dpb587/gget/releases/download/v0.5.0/gget-0.5.0-darwin-amd64"
-    sha256 "33bbbd685f8dc20a36d763e9165c406281d8a1c61d06e30dc9c9f3edcc522153"
+    url "https://github.com/dpb587/gget/releases/download/v0.5.2/gget-0.5.2-darwin-amd64"
+    sha256 "2b5f150526e626c96897f89ea9f3e84f05e02ff52bde02c29280d6f199a65992"
   elsif OS.linux?
-    url "https://github.com/dpb587/gget/releases/download/v0.5.0/gget-0.5.0-linux-amd64"
-    sha256 "999213d32c20c44214a3160a23d521f63379e52cbd9379a8c081df9720678a3d"
+    url "https://github.com/dpb587/gget/releases/download/v0.5.2/gget-0.5.2-linux-amd64"
+    sha256 "c8758e0f25eec1ef53c0f120a67e248c76c1b25d044fe361f992b0cb777d9407"
   end
 
   def install
